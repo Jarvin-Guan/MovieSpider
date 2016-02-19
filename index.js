3f0f1f8d2f72d7009a860b719fb16d402550b77e
Jarvin <jarvin_g@126.com>
2016-02-19 21:09:55 +0800

    localhost

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var request=require('superagent');
 var http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 8000;
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text' });
-    var url = 'http://www.bd-film.com/zx/21627.htm';
-    request
-        .get(url)
-        .end(function(e, r){
-            res.end(r.text);
-        })
+var url = 'http://www.bd-film.com/zx/21627.htm';
+request
+    .get(url)
+    .end(function(e, r){
+        res.end(r.text);
+    })
 
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
