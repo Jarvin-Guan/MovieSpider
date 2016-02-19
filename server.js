95069a03f7ac252839c3ce139fc060c1cc061aaf
Jarvin <jarvin_g@126.com>
2016-02-19 19:32:14 +0800

    添加docker file

---
+++ server.js
@@ -0,0 +1,19 @@
+var http=require('http')
+var temp=0;
+var server=http.createServer(function(req,res)
+{
+    res.writeHead(200,{
+        'Content-Type':'text/plain'
+    })
+    if(temp==0)
+    {
+        temp=Math.random();
+    }
+    res.end('Head World!jarvin tt '+temp);
+})
+
+server.listen(8000,function(){
+    console.log('Docker DEMO with node');
+})/**
+ * Created by Jarvin-Guan on 16/2/19.
+ */
