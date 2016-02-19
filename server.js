4501be96632c8ce8255abc51f9afe5e80d4a2176
Jarvin <jarvin_g@126.com>
2016-02-19 19:52:54 +0800

    changed server

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-var http=require('http')
-var temp=0;
 var server=http.createServer(function(req,res)
 {
     res.writeHead(200,{
         'Content-Type':'text/plain'
     })
-    if(temp==0)
-    {
-        temp=Math.random();
-    }
-    res.end('Head World!jarvin tt '+temp);
+    res.end('Hello');
 })
 
 server.listen(8000,function(){
