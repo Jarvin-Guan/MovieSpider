c22488cbbea07be0e92fdd7ebcddf5c625270aed
Jarvin <jarvin_g@126.com>
2016-02-19 20:51:44 +0800

    changed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var request=require('superagent');
+var http = require('http');
 
-var url = 'http://www.bd-film.com/zx/21627.htm';
-request
-    .get(url)
-    .end(function(err, res){
-        console.log(res.text);
-    });
+const hostname = '127.0.0.1';
+const port = 8000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text' });
+    var url = 'http://www.bd-film.com/zx/21627.htm';
+    request
+        .get(url)
+        .end(function(e, r){
+            res.end(r.text);
+        })
+
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
