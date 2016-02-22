6fed034cc732d521eef19544feb71b2d7597694d
Jarvin <jarvin_g@126.com>
2016-02-23 00:30:41 +0800

    read file

--- models/mongodb.js
+++ models/mongodb.js
@@ -2,30 +2,9 @@
 var mongoose = require('mongoose');
 var fs=require('fs');
 
-function getAllFiles(dir, callback) {
-    var filesArr = [];
-    dir = ///$/.test(dir) ? dir : dir + '/';
-        (function dir(dirpath, fn) {
-            var files = Sys.fs.readdirSync(dirpath);
-            exports.async(files, function (item, next) {
-                var info = Sys.fs.statSync(dirpath + item);
-                if (info.isDirectory()) {
-                    dir(dirpath + item + '/', function () {
-                        next();
-                    });
-                } else {
-                    filesArr.push(dirpath + item);
-                    callback && callback(dirpath + item);
-                    next();
-                }
-            }, function (err) {
-                !err && fn && fn();
-            });
-        })(dir);
-    return filesArr;
-}
-
-console.log(getAllFiles('/Picture'));
+fs.readdir(__dirname + '/Picture/', function (err, files) {
+    console.log(files);
+};
 
 // 本地 mongodb链接地址
 var mongodbUri = 'mongodb://127.0.0.1:27017/DaoCloudDemo';
